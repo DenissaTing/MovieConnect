6f59fd687785d0d94ca5320bef4370ef423f008c
denissating <dting@wawanesa.com>
2020-01-19 16:25:22 -0500

    Updates

---
+++ precache-manifest.afb0d0af2c9fe2e2b6cb93bb260a8d6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87857a768c3544fd53518fd61569df93",
+    "url": "/MovieConnect/index.html"
+  },
+  {
+    "revision": "01cf796569daf486705d",
+    "url": "/MovieConnect/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6756962645441352ea7d",
+    "url": "/MovieConnect/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "01cf796569daf486705d",
+    "url": "/MovieConnect/static/js/2.3e080fcf.chunk.js"
+  },
+  {
+    "revision": "1d453b9ab6185d326c39b05bdb23a46a",
+    "url": "/MovieConnect/static/js/2.3e080fcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6756962645441352ea7d",
+    "url": "/MovieConnect/static/js/main.e7319fb2.chunk.js"
+  },
+  {
+    "revision": "4ad4eb5ace030ce0fd49",
+    "url": "/MovieConnect/static/js/runtime-main.1153578f.js"
+  },
+  {
+    "revision": "3a6c71eeee4fe10e7396966027a80060",
+    "url": "/MovieConnect/static/media/mapdblogo.3a6c71ee.png"
+  }
+]);
